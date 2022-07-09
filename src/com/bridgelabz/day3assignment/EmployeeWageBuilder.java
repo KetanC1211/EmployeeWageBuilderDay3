b5ae3344068fe7344f1854f65d261c9c1bff64bc
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-09 11:19:39 +0530

    Added Part time Emp

--- src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
+++ src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
@@ -1,18 +1,27 @@
 package com.bridgelabz.day3assignment;
 
 public class EmployeeWageBuilder {
-	public static final int IS_FULL_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
 	public static void main(String[] args) {
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random()*10)%3;
 		int empRatePerHour = 20;
-		int empHrs = 8;
-		int salary;	
-		if(empCheck == IS_FULL_TIME) {
-			salary = empRatePerHour*empHrs;
-			System.out.println("Salary is " + salary);
+		int salary;
+		int empHrs;
+		//Computation 
+		if(empCheck == IS_FULL_TIME)
+		{
+			empHrs = 8;
 		}
-		else {
-			System.out.println("Salary is 0");
+		else if(empCheck == IS_PART_TIME)
+		{
+			empHrs = 4;
 		}
+		else 
+		{
+			empHrs = 0;
+		}
+		salary = empRatePerHour*empHrs;
+		System.out.println("Salary is " + salary);
 	}
 }
