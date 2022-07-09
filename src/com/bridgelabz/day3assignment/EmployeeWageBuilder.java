2d416bba5f98257e40484fcf6cf51d5d6f06ae5a
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-09 11:43:33 +0530

    Added Absent and Present

--- src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
+++ src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
@@ -1,35 +1,14 @@
 package com.bridgelabz.day3assignment;
 
 public class EmployeeWageBuilder {
-	public static final int IS_FULL_TIME = 2;
-	public static final int IS_PART_TIME = 1;
-	public static final int NUM_OF_WORKING_DAY= 20;
-	public static final int MAXHRS = 100;
+	public static final int IS_FULL_TIME = 1;
 	public static void main(String[] args) {
-		int empRatePerHour = 20;
-		int salary;
-		int empHrs;
-		int totalSalary = 0,totalEmpHrs = 0;
-		int totalWorkingdays = 0; 
-		while(totalEmpHrs <= MAXHRS && totalWorkingdays <= NUM_OF_WORKING_DAY)
-		{ 
-		double empCheck = Math.floor(Math.random()*10)%3;
-		switch((int)empCheck)
-		{
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs =0;
+		double empCheck = Math.floor(Math.random()*10)%2;	
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
 		}
-		salary = empRatePerHour*empHrs;
-		totalSalary = totalSalary + salary;
-		totalEmpHrs = totalEmpHrs + empHrs;
-		totalWorkingdays++;
+		else {
+			System.out.println("Employee is Absent");
 		}
-		System.out.println("Monthly wage is " + totalSalary);
 	}
 }
