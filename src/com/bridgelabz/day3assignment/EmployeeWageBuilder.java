dc11146b334956912000a14368f7cfb3812406ec
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-09 11:32:30 +0530

    Added Monthly Wage

--- src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
+++ src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
@@ -3,12 +3,15 @@ package com.bridgelabz.day3assignment;
 public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 2;
 	public static final int IS_PART_TIME = 1;
+	public static final int NUM_OF_WORKING_DAY= 20;
 	public static void main(String[] args) {
-		double empCheck = Math.floor(Math.random()*10)%3;
 		int empRatePerHour = 20;
 		int salary;
 		int empHrs;
-		//Computation 
+		int totalSalary = 0;
+		for(int i=1;i<=NUM_OF_WORKING_DAY;i++)
+		{ 
+		double empCheck = Math.floor(Math.random()*10)%3;
 		switch((int)empCheck)
 		{
 		case IS_FULL_TIME:
@@ -21,6 +24,8 @@ public class EmployeeWageBuilder {
 			empHrs =0;
 		}
 		salary = empRatePerHour*empHrs;
-		System.out.println("Salary is " + salary);
+		totalSalary = totalSalary + salary;
+		}
+		System.out.println("Monthly wage is " + totalSalary);
 	}
 }
