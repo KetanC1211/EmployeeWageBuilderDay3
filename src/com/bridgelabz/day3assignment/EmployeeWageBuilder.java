679a4dbaaec985edd5a94bb5c11e51ffde04d110
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-09 11:09:56 +0530

    Refactored UC1

--- src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
+++ src/com/bridgelabz/day3assignment/EmployeeWageBuilder.java
@@ -3,12 +3,16 @@ package com.bridgelabz.day3assignment;
 public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 1;
 	public static void main(String[] args) {
-		double empCheck = Math.floor(Math.random()*10)%2;	
+		double empCheck = Math.floor(Math.random()*10)%2;
+		int empRatePerHour = 20;
+		int empHrs = 8;
+		int salary;	
 		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+			salary = empRatePerHour*empHrs;
+			System.out.println("Salary is " + salary);
 		}
 		else {
-			System.out.println("Employee is Absent");
+			System.out.println("Salary is 0");
 		}
 	}
 }
